2c295d40116b7580c68a16cf83a5d5292fa990b9
Polok Sarker <poloksarker580@gmail.com>
2026-08-30 11:44:26 +0600

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int a,b,sum=0;
+
+    printf("a=");
+    scanf ("%d",&a);
+    
+    printf("b=");
+    scanf ("%d",&b);
+
+    {
+        sum=a+b;
+    }
+     printf("Sum=%d",sum);
+     return 0;
+}
