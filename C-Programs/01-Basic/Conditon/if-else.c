2c36dfd4486ca300feee3ccb1fa5a486f80f99cf
Polok Sarker <poloksarker580@gmail.com>
2026-09-06 23:06:43 +0600

    Create if-else.c

---
+++ C-Programs/01-Basic/Conditon/if-else.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+  int time = 16;
+
+bool isMorning = time < 12;
+bool isDay = time < 18;
+
+if (isMorning) {
+  printf("Good morning.");
+} else if (isDay) {
+  printf("Good day.");
+} else {
+  printf("Good evening.");
+}
+  return 0;
+}
