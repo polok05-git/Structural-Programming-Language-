67a01bfb32fa080953f3bd1462aca749ff845217
Polok Sarker <poloksarker580@gmail.com>
2026-09-06 23:35:23 +0600

    Add if-else age check program in C

---
+++ C-Programs/02-Problem-Solving/Condition/if-else.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+  int age;
+  printf("Enter Age=");
+  scanf("%d",&age);
+  if( age >= 18){
+    printf("Allowed");
+  }
+  else{
+    printf("Not Allowed");
+  }
+  return 0;
+}
