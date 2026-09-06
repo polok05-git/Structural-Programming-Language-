8a7114ea09723b1437e9f07d94d99eaac43a75fc
Polok Sarker <poloksarker580@gmail.com>
2026-09-06 22:59:16 +0600

    Add if statement to compare two integers

---
+++ C-Programs/01-Basic/Conditon/if.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int x = 20;
+    int y = 18;
+
+    bool isGreater = x > y;
+
+    if (isGreater) {
+        printf("x is greater than y");
+     }
+ return 0;
+}
