1704501a6a34038bcfa29393e76b70a63af02bba
Polok Sarker <poloksarker580@gmail.com>
2026-09-09 10:53:48 +0600

    Create Switch.c

---
+++ C-Programs/01-Basic/Switch.c
@@ -0,0 +1,18 @@
+#include<studio.h>
+int main(){
+
+
+int day = 4;
+
+switch (day) {
+  case 6:
+    printf("Today is Saturday");
+    break;
+  case 7:
+    printf("Today is Sunday");
+    break;
+  default:
+    printf("Looking forward to the Weekend");
+}
+}
+// Outputs "Looking forward to the Weekend"
