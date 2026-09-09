13ae3cf7d286df61fcb9cadb505ed8f16fb5975a
Polok Sarker <poloksarker580@gmail.com>
2026-09-09 21:07:13 +0600

    Update Primenum/primenum.c

--- C-Programs/02-Problem-Solving/primenum.c
+++ C-Programs/02-Problem-Solving/primenum.c
@@ -10,7 +10,7 @@ int main() {
         prime = 0;
     } else {
         for (i = 2; i * i <= num; i++) {
-            if (num % i == 0) {
+           if (num % i == 0) {
                 prime = 0;
                 break;
             }
