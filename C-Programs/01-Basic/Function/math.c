3decfe27d6b733d18cd1e216400dcfea1f30e238
Polok Sarker <poloksarker580@gmail.com>
2026-09-20 23:02:42 +0600

    Add math.c for basic mathematical operations

---
+++ C-Programs/01-Basic/Function/math.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    printf("%f", sqrt(16));
+    printf("%f", ceil(1.4));
+    printf("%f", floor(1.4));
+    printf("%f", pow(4, 3));
+  return 0;
+}
