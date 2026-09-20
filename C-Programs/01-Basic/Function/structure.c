58c98d2ebdf9cbdb3594d8c31e2c337cf6cfed45
Polok Sarker <poloksarker580@gmail.com>
2026-09-20 22:38:34 +0600

    Add function to calculate and print sum of two numbers

---
+++ C-Programs/01-Basic/Function/structure.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// Create a function
+void calculateSum() {
+  int x = 5;
+  int y = 10;
+  int sum = x + y;
+  printf("The sum of x + y is: %d", sum);
+}
+
+int main() {
+  calculateSum();  // call the function
+  return 0;
+}
